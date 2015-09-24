7e628af59da5e0482c05ce63ad397319a67ac50a
Ziggy Maes <ziggy.maes@student.howest.be>
2015-09-24 11:39:08 +0200

    Initial commit: opgave

---
+++ Week 1/StudentModulePuntWS/StudentModulePuntWS/StudentModulePuntWS/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudentModulePuntWS.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ Week 1/StudentModulePuntWS/StudentModulePuntWS/StudentModulePuntWS/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace StudentModulePuntWS.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
