aa7707fbdf8b558a9d4a4e66dcee20d5cbb82a50
Ziggy Maes <ziggy.maes@student.howest.be>
2015-10-29 10:00:22 +0100

    Project init // Design // Init properties StudentModulePunt

---
+++ Week 1/Herhaling/StudentModulePuntApp/Models/StudentModulePunt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentModulePuntApp.Models
+{
+    public class StudentModulePunt
+    {
+        public string Email { get; set; }
+        public string Geboorteplaats { get; set; }
+        public string Module { get; set; }
+        public int Punt { get; set; }
+    }
+}
