fc089bfe2ae6c568f99eeb602e6a17830be70f55
Ziggy Maes <ziggy.maes@student.howest.be>
2015-10-29 11:31:26 +0100

    Fill upper combobox with email adresses

--- Week 1/Herhaling/StudentModulePuntApp/MainPage.xaml.cs
+++ Week 1/Herhaling/StudentModulePuntApp/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using StudentModulePuntApp.Models;
+using StudentModulePuntApp.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -10,10 +13,31 @@ namespace StudentModulePuntApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        List<StudentModulePunt> _smp;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            this.InitializeComponent();     
         }
+        private async void LoadScores()
+        {
+            _smp = await StudentModulePuntRepository.Entries();
+            cmbStudent.ItemsSource = StudentEmail();
+        }
+        private void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            LoadScores();
+        }
+        private List<string> StudentEmail()
+        {
+            List<string> EmailList = new List<string>();
+            //StudentModulePunt smp = new StudentModulePunt();
+            foreach (StudentModulePunt s in _smp)
+            {
+                EmailList.Add(s.Email);
+            }
 
+            return EmailList;
+        }
     }
 }
--- Week 1/Herhaling/StudentModulePuntApp/Repositories/StudentModulePuntRepository.cs
+++ Week 1/Herhaling/StudentModulePuntApp/Repositories/StudentModulePuntRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using StudentModulePuntApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
