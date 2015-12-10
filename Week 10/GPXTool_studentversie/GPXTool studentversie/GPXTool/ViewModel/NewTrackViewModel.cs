4a6cdc7f075b007980e315088624ce29f513b0ac
Ziggy Maes <ziggy.maes@student.howest.be>
2015-12-10 09:58:44 +0100

    Week 10 stage files

---
+++ Week 10/GPXTool_studentversie/GPXTool studentversie/GPXTool/ViewModel/NewTrackViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GalaSoft.MvvmLight.Command;
+using GPXTool.Data;
+using GPXTool.Models;
+using GPXTool.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace GPXTool.ViewModel
+{
+    public class NewTrackViewModel : ViewModelBaseClass
+    {
+    }
+}
